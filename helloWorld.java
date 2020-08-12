ee0b544ca3da39a8fcd9df3778ea38c506648c70
Mohamed Hussein Karjiker <mhkarj@gmail.com>
2020-08-12 17:47:43 +0200

    Created a user input

--- helloWorld.java
+++ helloWorld.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class helloWorld {
 
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Git is Awesome!");
+		System.out.println("Enter Message here: ");
+		Scanner s = new Scanner(System.in);
+		String user_str = s.nextLine();
 		// TODO Auto-generated method stub
 
 	}
